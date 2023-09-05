9e37cc2fded19c3eddf9d49020118e2426762af0
김수찬 <tncks097@naver.com>
2023-09-06 01:00:53 +0900

    Feat : 스토리 박스 | 스토리 기능 추가

--- src/backend/src/main/java/com/spring/demo/domain/storybox/dto/StoryBoxBuildDTO.java
+++
@@ -1,4 +0,0 @@
-package com.spring.demo.domain.storybox.dto;
-
-public class StoryBoxBuildDTO {
-}
--- src/backend/src/main/java/com/spring/demo/domain/storybox/dto/StoryBuildDTO.java
+++
@@ -1,9 +0,0 @@
-package com.spring.demo.domain.storybox.dto;
-
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
-@NoArgsConstructor
-public class StoryBuildDTO {
-}
