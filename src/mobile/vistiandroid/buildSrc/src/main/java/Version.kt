d108c2eccd6839786f38ceb8d7414fed321a867a
leehandsub <gustjq7027@naver.com>
2023-09-06 17:50:25 +0900

    chore : module 새롭게 생성

--- src/mobile/vistiandroid/buildSrc/src/main/java/Version.kt
+++ src/mobile/vistiandroid/buildSrc/src/main/java/Version.kt
@@ -41,7 +41,7 @@ object Dependencies {
     object Test {
         const val JUNIT = "junit:junit:4.13.2"
         const val TEST_EXT = "androidx.test.ext:junit:1.1.5"
-        const val ESPRESSO_CORE = "androidx.test.espresso:espresso-core:3.4.1"
+        const val ESPRESSO_CORE = "androidx.test.espresso:espresso-core:3.5.1"
     }
 
     object RETROFIT {
--- src/mobile/vistiandroid/domain/src/androidTest/java/com/ssafy/domain/ExampleInstrumentedTest.kt
+++ src/mobile/vistiandroid/domain/src/androidTest/java/com/ssafy/domain/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.ssafy.domain", appContext.packageName)
+        assertEquals("com.ssafy.domain.test", appContext.packageName)
     }
 }
