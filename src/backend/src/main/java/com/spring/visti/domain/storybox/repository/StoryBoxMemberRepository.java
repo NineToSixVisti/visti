7feac787be99002ab8e4b5d167d39543b125fb0f
김수찬 <tncks097@naver.com>
2023-09-08 02:53:40 +0900

    feat : validate 및 generate  link 구현

--- src/backend/src/main/java/com/spring/visti/domain/storybox/repository/StoryBoxMemberRepository.java
+++ src/backend/src/main/java/com/spring/visti/domain/storybox/repository/StoryBoxMemberRepository.java
@@ -1,9 +1,14 @@
 package com.spring.visti.domain.storybox.repository;
 
+import com.spring.visti.domain.member.entity.Member;
+import com.spring.visti.domain.storybox.entity.StoryBox;
 import com.spring.visti.domain.storybox.entity.StoryBoxMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StoryBoxMemberRepository extends JpaRepository<StoryBoxMember, Long> {
 
+    Optional<StoryBoxMember> findByStoryBoxAndMember(StoryBox storyBox, Member member);
 
 }
