b699c97d75d6ef94f28eb543e2744e8bcff35d09
김수찬 <tncks097@naver.com>
2023-09-08 23:31:30 +0900

    feat : DefaultService 추가
    - 데이터 관련하여 getMember, getStory, getStoryBox 의 사용량이 많아 Default 서비스에 일괄 적용하였습니다.

---
+++ src/backend/src/main/java/com/spring/visti/api/common/service/DefaultService.java
@@ -0,0 +1,37 @@
+package com.spring.visti.api.common.service;
+
+import com.spring.visti.domain.member.entity.Member;
+import com.spring.visti.domain.member.repository.MemberRepository;
+import com.spring.visti.domain.storybox.entity.Story;
+import com.spring.visti.domain.storybox.entity.StoryBox;
+import com.spring.visti.domain.storybox.repository.StoryBoxRepository;
+import com.spring.visti.domain.storybox.repository.StoryRepository;
+import com.spring.visti.utils.exception.ApiException;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+import static com.spring.visti.utils.exception.ErrorCode.*;
+
+
+public interface DefaultService {
+
+    default Member getMember(String email, MemberRepository memberRepository) {
+        Optional<Member> optionalMember = memberRepository.findByEmail(email);
+        if (optionalMember.isEmpty()) { throw new ApiException(NO_MEMBER_ERROR); }
+        return optionalMember.get();
+    }
+
+    default Story getStory(Long storyId, StoryRepository storyRepository) {
+        Optional<Story> optionalStory = storyRepository.findById(storyId);
+        if (optionalStory.isEmpty()) { throw new ApiException(NO_STORY_ERROR); }
+        return optionalStory.get();
+    }
+
+    default StoryBox getStoryBox(Long storyBoxId, StoryBoxRepository storyBoxRepository) {
+        Optional<StoryBox> optionalStoryBox = storyBoxRepository.findById(storyBoxId);
+        if (optionalStoryBox.isEmpty()) { throw new ApiException(NO_STORY_BOX_ERROR); }
+        return optionalStoryBox.get();
+    }
+}
