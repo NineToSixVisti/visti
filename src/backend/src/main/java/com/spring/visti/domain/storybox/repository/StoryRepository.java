2bd9b813031d94ebce5017f6963b4967fac5e012
김수찬 <tncks097@naver.com>
2023-09-08 23:35:37 +0900

    feat : 페이지네이션 기능 추가

--- src/backend/src/main/java/com/spring/visti/domain/storybox/repository/StoryRepository.java
+++ src/backend/src/main/java/com/spring/visti/domain/storybox/repository/StoryRepository.java
@@ -2,10 +2,12 @@ package com.spring.visti.domain.storybox.repository;
 
 import com.spring.visti.domain.member.entity.Member;
 import com.spring.visti.domain.storybox.entity.Story;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface StoryRepository extends JpaRepository<Story, Long> {
-    List<Story> findByMember(Member member);
+    Page<Story> findByMember(Member member, Pageable pageable);
 }
