fa2bdd419f8817eeca210e02b8249f6550e1d6c9
오희주 <noion0511@gmail.com>
2023-09-08 21:42:07 +0900

    update : 프로필 화면의 내 프로필 수정
    - 컴포넌트 정렬 수정
    - 프로필 사진이 없을 시, 디폴트 이미지 상태 추가
    - toolbar text font weight 수정
    - 사용하지 않는 코드 삭제 및 코드 정렬

--- src/mobile/vistiandroid/presentation/src/main/java/com/ssafy/presentation/ui/profile/ProfileScreen.kt
+++ src/mobile/vistiandroid/presentation/src/main/java/com/ssafy/presentation/ui/profile/ProfileScreen.kt
@@ -1,15 +1,21 @@
 package com.ssafy.presentation.ui.profile
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Card
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -17,17 +23,24 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.ssafy.domain.model.LikeSortType
+import com.ssafy.presentation.R
 import com.ssafy.presentation.ui.profile.component.StoryLazyVerticalGrid
-import com.ssafy.presentation.ui.like.ImageListViewModel
-import com.ssafy.presentation.ui.profile.component.MyPageProfileImg
 import com.ssafy.presentation.ui.profile.component.ToolbarWithProfile
+import com.ssafy.presentation.ui.theme.VistiAndroidTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ProfileScreen(viewModel: ImageListViewModel = hiltViewModel()) {
+fun ProfileScreen(viewModel: ProfileViewModel = hiltViewModel()) {
     val state = viewModel.state.value
     when {
         state.error.isNotBlank() -> {
@@ -56,18 +69,128 @@ fun ProfileScreen(viewModel: ImageListViewModel = hiltViewModel()) {
             ) { innerPadding ->
                 Column(
                     modifier = Modifier
-                        .padding(innerPadding),
-                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                        .padding(innerPadding)
                 ) {
-                    Row(modifier = Modifier.padding(8.dp)) {
-                        Column {
-                            MyPageProfileImg("")
-                            Text("noion0511", fontSize = 12.sp)
-                        }
-                    }
+                    Spacer(modifier = Modifier.height(4.dp))
+                    ProfileSection()
+                    Spacer(modifier = Modifier.height(25.dp))
                     StoryLazyVerticalGrid(state.images)
                 }
             }
         }
+    }
+}
+
+@Composable
+fun ProfileSection() {
+    Column(Modifier.fillMaxWidth()) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)
+        ) {
+            ImageSection(
+                image = painterResource(id = R.drawable.ic_profile),
+                modifier = Modifier
+                    .size(100.dp)
+                    .weight(3f)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            StatSection(modifier = Modifier.weight(7f))
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+        ProfileDescription(
+            displayName = "ohhuiju"
+        )
+    }
+}
+
+@Composable
+fun ImageSection(
+    image: Painter,
+    modifier: Modifier = Modifier
+) {
+    Image(
+        painter = image,
+        contentDescription = null,
+        modifier = modifier
+            .aspectRatio(1f, matchHeightConstraintsFirst = true)
+            .border(
+                width = 1.dp,
+                color = Color.LightGray,
+                shape = CircleShape
+            )
+            .clip(CircleShape)
+            .background(Color.LightGray)
+            .padding(10.dp)
+
+    )
+}
+
+@Composable
+fun StatSection(modifier: Modifier = Modifier) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround,
+        modifier = modifier
+    ) {
+        ProfileStat(numberText = "1,000", text = "스토리")
+        ProfileStat(numberText = "34", text = "스토리 상자")
+    }
+}
+
+@Composable
+fun ProfileDescription(
+    displayName: String
+) {
+    val letterSpacing = 0.5.sp
+    val lineHeight = 20.sp
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 20.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = displayName,
+            fontWeight = FontWeight.Medium,
+            letterSpacing = letterSpacing,
+            lineHeight = lineHeight
+        )
+        Image(
+            painter = painterResource(id = R.drawable.ic_check),
+            contentDescription = LikeSortType.UP.name,
+            modifier = Modifier.padding(start = 3.dp)
+        )
+    }
+}
+
+@Composable
+fun ProfileStat(
+    numberText: String,
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Text(
+            text = numberText,
+            fontWeight = FontWeight.Medium,
+            fontSize = 20.sp
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(text = text)
+    }
+}
+
+@Preview
+@Composable
+internal fun PreviewMainContainer() {
+    VistiAndroidTheme {
+        ProfileScreen()
     }
 }
--- src/mobile/vistiandroid/presentation/src/main/java/com/ssafy/presentation/ui/profile/component/ToolbarWithProfile.kt
+++ src/mobile/vistiandroid/presentation/src/main/java/com/ssafy/presentation/ui/profile/component/ToolbarWithProfile.kt
@@ -4,13 +4,14 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ToolbarWithProfile() {
     TopAppBar(
-        title = { Text("noion0511@gmail.com", fontSize = 16.sp) },
+        title = { Text("noion0511@gmail.com", fontSize = 16.sp, fontWeight = FontWeight.Medium) },
         actions = {
             ToolbarWithProfileActions()
         }
