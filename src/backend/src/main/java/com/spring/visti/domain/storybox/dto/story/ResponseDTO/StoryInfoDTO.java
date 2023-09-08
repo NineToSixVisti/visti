fd73b6115a7f76b1fb69fea4acd48ba5249a0867
김수찬 <tncks097@naver.com>
2023-09-08 23:27:38 +0900

    refact : story 엔티티 변경에 따른 dto 변경

--- src/backend/src/main/java/com/spring/visti/domain/storybox/dto/story/ResponseDTO/StoryInfoDTO.java
+++ src/backend/src/main/java/com/spring/visti/domain/storybox/dto/story/ResponseDTO/StoryInfoDTO.java
@@ -1,34 +1,61 @@
 package com.spring.visti.domain.storybox.dto.story.ResponseDTO;
 
+import com.spring.visti.domain.member.dto.ResponseDTO.MemberExposedDTO;
 import com.spring.visti.domain.member.entity.Member;
+import com.spring.visti.domain.storybox.constant.StoryType;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
 public class StoryInfoDTO {
 
-    private Member member;
-    private String letter_path;
-    private String image_path;
-    private String audio_path;
-    private String video_path;
+    private Long id;
+    private Long storyBoxId;
+    private MemberExposedDTO member;
+
+
+    private StoryType main_file_type;
+    private String main_file_path;
+
+    private StoryType sub_file_type;
+    private String sub_file_path;
+
+
+    private Boolean blind;
+    private LocalDateTime created_at;
+    private LocalDateTime finish_at;
+    private Boolean like;
 
     private String secret_key;
     private String nft_hash;
 
     @Builder
-    public StoryInfoDTO(Member member, String letter_path,
-                        String image_path, String audio_path, String video_path,
-                        String secret_key, String nft_hash){
+    public StoryInfoDTO(Long id, Long storyBoxId, Member member,
+                        StoryType main_file_type, String main_file_path,
+                        StoryType sub_file_type, String sub_file_path,
+                        Boolean blind, LocalDateTime finish_at, LocalDateTime created_at, Boolean like,
+                        String secret_key, String nft_hash
+    ){
+        this.id = id;
+        this.storyBoxId = storyBoxId;
+        this.member = MemberExposedDTO.of(member);
 
-        this.member = member;
-        this.letter_path = letter_path;
-        this.image_path = image_path;
-        this.audio_path = audio_path;
-        this.video_path = video_path;
-//        this.secret_key = secret_key;
+        this.main_file_type = main_file_type;
+        this.main_file_path = main_file_path;
+
+        this.sub_file_type = sub_file_type;
+        this.sub_file_path = sub_file_path;
+
+        this.blind = blind;
+        this.created_at = created_at;
+        this.finish_at = finish_at;
+        this.like = like;
+
+        this.secret_key = secret_key;
         this.nft_hash = nft_hash;
     }
 }
