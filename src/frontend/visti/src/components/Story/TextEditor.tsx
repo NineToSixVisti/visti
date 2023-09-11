cef9cce0b129660c3627372926352f1664735a8a
김상진 <tkdwls0418@naver.com>
2023-09-11 17:22:35 +0900

    feat: FontSizeEditor및 ColorEditor 기능 추가

--- src/frontend/visti/src/components/Story/TextEditor.tsx
+++ src/frontend/visti/src/components/Story/TextEditor.tsx
@@ -1,47 +1,64 @@
 import React, { useState } from 'react';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css'; 
+import styled from 'styled-components';
+import FontSize from './FontSizeEditor';
+import ColorEditor from './ColorEditor';
+import Draggable from 'react-draggable'; 
+
+const EditorContainer = styled.div`
+  position: relative;
+`;
+
+const StyledTextarea = styled.textarea<{ fontSize: number, color: string }>`
+  background: transparent;
+  border: 1px solid #ccc;
+  width: 300px;
+  height: 100px;
+  font-size: ${props => props.fontSize}px;
+  color: ${props => props.color};
+`;
+
+const FinalText = styled.div<{ fontSize: number, color: string }>`
+  font-size: ${props => props.fontSize}px;
+  color: ${props => props.color};
+`;
 
 const TextEditor: React.FC = () => {
-  const [editorVisible, setEditorVisible] = useState(false);
-  const [text, setText] = useState('');
-  const [fontSize, setFontSize] = useState('14px');
+  const [editorText, setEditorText] = useState('');
+  const [finalText, setFinalText] = useState('');
+  const [fontSize, setFontSize] = useState(14);
   const [color, setColor] = useState('#000000');
+  const [editorVisible, setEditorVisible] = useState(false);
 
-  const modules = {
-    toolbar: [
-      [{ 'header': '1'}, {'header': '2'}, { 'font': [] }],
-      [{size: []}],
-      ['bold', 'italic', 'underline'],
-      [{'list': 'ordered'}, {'list': 'bullet'}],
-      [{'align': []}],
-      ['color'],
-      ['clean']                                         
-    ],
+  const handleComplete = () => {
+    setFinalText(editorText);
+    setEditorVisible(false);
   };
 
   return (
-    <div style={{ position: 'relative' }}>
-      <button onClick={() => setEditorVisible(!editorVisible)}> T</button>
-      {editorVisible && (
-        <div style={{ position: 'absolute', top: '50px', width: '300px' }}>
-          <ReactQuill 
-            value={text} 
-            onChange={setText} 
-            modules={modules}
-            style={{ fontSize, color }}
-          />
-          <div>
-            <label>글자 크기: </label>
-            <input type="number" value={parseInt(fontSize, 10)} onChange={(e) => setFontSize(`${e.target.value}px`)} />
-          </div>
-          <div>
-            <label>글자 색상: </label>
-            <input type="color" value={color} onChange={(e) => setColor(e.target.value)} />
-          </div>
-        </div>
-      )}
-    </div>
+    <EditorContainer>
+        <button onClick={() => setEditorVisible(!editorVisible)}>T</button>
+        {editorVisible && (
+            <>
+                <ColorEditor setColor={setColor} currentColor={color} />
+                <FontSize setFontSize={setFontSize} />
+                <button onClick={handleComplete}>완료</button>
+                <StyledTextarea 
+                    value={editorText} 
+                    onChange={(e) => setEditorText(e.target.value)} 
+                    fontSize={fontSize}
+                    color={color}
+                />
+            </>
+        )}
+     
+        {!editorVisible && (
+            <Draggable>
+                <FinalText fontSize={fontSize} color={color}>
+                    {finalText}
+                </FinalText>
+            </Draggable>
+        )}
+    </EditorContainer>
   );
 }
 
