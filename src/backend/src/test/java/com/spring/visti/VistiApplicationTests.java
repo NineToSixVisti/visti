dc40ef3a2f3b0af5a04840a8e878df32bb9e3d09
김수찬 <tncks097@naver.com>
2023-09-12 09:31:18 +0900

    nothing changed

--- src/backend/src/test/java/com/spring/visti/VistiApplicationTests.java
+++ src/backend/src/test/java/com/spring/visti/VistiApplicationTests.java
@@ -1,15 +1,8 @@
 package com.spring.visti;
 
-import com.spring.visti.api.member.service.MemberService;
-import com.spring.visti.domain.member.dto.RequestDTO.MemberJoinDTO;
-import com.spring.visti.domain.member.dto.RequestDTO.MemberLoginDTO;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
