4bce71c7de7f86d4763be623ca7dc6fa99ae4dc9
오희주 <noion0511@gmail.com>
2023-09-13 15:01:16 +0900

    feat : 회원탈퇴 및 로그아웃 레이아웃(#S09P22D102-279)
    - 커스텀 다이얼로그 생성

--- src/mobile/vistiandroid/presentation/src/main/java/com/ssafy/presentation/ui/setting/UserAccountScreen.kt
+++ src/mobile/vistiandroid/presentation/src/main/java/com/ssafy/presentation/ui/setting/UserAccountScreen.kt
@@ -9,6 +9,8 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -16,22 +18,21 @@ import androidx.navigation.NavController
 import com.ssafy.presentation.ui.profile.ProfileViewModel
 import com.ssafy.presentation.ui.setting.component.BackToolbar
 import com.ssafy.presentation.ui.setting.component.DetailSettingButton
+import com.ssafy.presentation.ui.common.VistiDialog
 import com.ssafy.presentation.ui.theme.Black
 import com.ssafy.presentation.ui.theme.PrimaryColor
 import com.ssafy.presentation.ui.theme.White
 
 @Composable
 fun UserAccountScreen(
-    navController: NavController,
-    viewModel: ProfileViewModel = hiltViewModel()
+    navController: NavController, viewModel: ProfileViewModel = hiltViewModel()
 ) {
     val state = viewModel.state.value
 
     when {
         state.error.isNotBlank() -> {
             Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
+                modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
             ) {
                 Text(text = state.error)
             }
@@ -39,24 +40,23 @@ fun UserAccountScreen(
 
         state.isLoading -> {
             Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
+                modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
             ) {
                 CircularProgressIndicator()
             }
         }
 
         else -> {
-            Scaffold(
-                topBar = {
-                    BackToolbar(text = "정보") {
-                        navController.popBackStack()
-                    }
+            val signOutState = remember { mutableStateOf(false) }
+            val logOutState = remember { mutableStateOf(false) }
+
+            Scaffold(topBar = {
+                BackToolbar(text = "정보") {
+                    navController.popBackStack()
                 }
-            ) { innerPadding ->
+            }) { innerPadding ->
                 Column(
-                    modifier = Modifier
-                        .padding(innerPadding)
+                    modifier = Modifier.padding(innerPadding)
                 ) {
 
                     val colorState = if (isSystemInDarkTheme()) {
@@ -72,10 +72,28 @@ fun UserAccountScreen(
 
                     }
                     DetailSettingButton("계정 로그아웃", PrimaryColor) {
-
+                        logOutState.value = true
                     }
                     DetailSettingButton("계정 회원탈퇴", PrimaryColor) {
+                        signOutState.value = true
+                    }
 
+                    val isDarkTheme = isSystemInDarkTheme()
+
+                    if (logOutState.value) {
+                        VistiDialog(onDismissRequest = { logOutState.value = false },
+                            onConfirmation = { logOutState.value = false },
+                            "회원 탈퇴하시겠습니까?",
+                            isDarkTheme
+                        )
+                    }
+
+                    if (signOutState.value) {
+                        VistiDialog(onDismissRequest = { signOutState.value = false },
+                            onConfirmation = { signOutState.value = false },
+                            "회원 탈퇴하시겠습니까?",
+                            isDarkTheme
+                        )
                     }
                 }
             }
--- src/mobile/vistiandroid/presentation/src/main/java/com/ssafy/presentation/ui/theme/Theme.kt
+++ src/mobile/vistiandroid/presentation/src/main/java/com/ssafy/presentation/ui/theme/Theme.kt
@@ -17,15 +17,15 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80,
+    primary = PrimaryColor,
+    secondary = SecondaryColor,
+    tertiary = LightBackgroundColor,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40,
+    primary = PrimaryColor,
+    secondary = SecondaryColor,
+    tertiary = DarkBackgroundColor,
 )
 
 @Composable
