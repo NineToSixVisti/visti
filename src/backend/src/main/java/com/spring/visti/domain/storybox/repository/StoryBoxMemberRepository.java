fef41b9061c5d52dc38433b998c872ab243ff674
김수찬 <tncks097@naver.com>
2023-09-13 23:36:48 +0900

    feat : 마이페이지에 내가 만든 방 보이게 추가

--- src/backend/src/main/java/com/spring/visti/domain/storybox/repository/StoryBoxMemberRepository.java
+++ src/backend/src/main/java/com/spring/visti/domain/storybox/repository/StoryBoxMemberRepository.java
@@ -2,6 +2,7 @@ package com.spring.visti.domain.storybox.repository;
 
 import com.spring.visti.domain.member.entity.Member;
 import com.spring.visti.domain.member.entity.MemberLikeStory;
+import com.spring.visti.domain.storybox.constant.Position;
 import com.spring.visti.domain.storybox.entity.StoryBox;
 import com.spring.visti.domain.storybox.entity.StoryBoxMember;
 import org.springframework.data.domain.Page;
@@ -13,6 +14,8 @@ import java.util.Optional;
 public interface StoryBoxMemberRepository extends JpaRepository<StoryBoxMember, Long> {
 
     Page<StoryBoxMember> findByMember(Member member, Pageable pageable);
+
+    Page<StoryBoxMember> findByMemberAndPosition(Member member, Position position, Pageable pageable);
 
     Optional<StoryBoxMember> findByStoryBoxIdAndMember(Long storyBoxId, Member member);
 }
