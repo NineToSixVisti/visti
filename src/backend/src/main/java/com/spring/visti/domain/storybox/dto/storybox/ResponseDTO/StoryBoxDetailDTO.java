2a869f4b9e0b81f84955f5a91d9359a4d7b76c30
김수찬 <tncks097@naver.com>
2023-09-13 23:41:56 +0900

    fix : 스토리박스 디테일누락되는 부분  수정

--- src/backend/src/main/java/com/spring/visti/domain/storybox/dto/storybox/ResponseDTO/StoryBoxDetailDTO.java
+++ src/backend/src/main/java/com/spring/visti/domain/storybox/dto/storybox/ResponseDTO/StoryBoxDetailDTO.java
@@ -1,6 +1,6 @@
 package com.spring.visti.domain.storybox.dto.storybox.ResponseDTO;
 
-import com.spring.visti.domain.member.entity.Member;
+import com.spring.visti.domain.member.dto.ResponseDTO.MemberExposedDTO;
 import com.spring.visti.domain.storybox.entity.StoryBox;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,7 +14,7 @@ public class StoryBoxDetailDTO {
 
     private String box_img_path;
     private String name;
-    private Member creator;
+    private MemberExposedDTO creator;
     private String detail;
 
     private LocalDateTime create_at;
@@ -26,25 +26,40 @@ public class StoryBoxDetailDTO {
     private Boolean blind;
 
     @Builder
-    public StoryBoxDetailDTO(/*Member creator, String box_img_path, String name, Boolean blind,
-                             String detail, Integer member_num, Integer story_num,
-                             LocalDateTime create_at, LocalDateTime finish_at*/
-            String detail, Integer member_num, Integer story_num
+    public StoryBoxDetailDTO(
+            String box_img_path, String name, String detail, MemberExposedDTO creator,
+            Boolean blind, Integer member_num, Integer story_num,
+            LocalDateTime create_at, LocalDateTime finish_at
                             ){
-
+        this.box_img_path = box_img_path;
+        this.name = name;
         this.detail = detail;
+        this.creator = creator;
+
+        this.blind = blind;
         this.member_num = member_num;
         this.story_num = story_num;
+
+        this.create_at = create_at;
+        this.finish_at = finish_at;
     }
 
     public static StoryBoxDetailDTO toDetailDTO(StoryBox storyBox){
+        MemberExposedDTO creatorInfo = MemberExposedDTO.of(storyBox.getCreator());
+
         Integer member_num = storyBox.getStoryBoxMembers().size();
         Integer story_num = storyBox.getStories().size();
 
         return StoryBoxDetailDTO.builder()
+                .box_img_path(storyBox.getBox_img_path())
+                .name(storyBox.getName())
                 .detail(storyBox.getDetail())
+                .creator(creatorInfo)
+                .blind(storyBox.getBlind())
                 .member_num(member_num)
                 .story_num(story_num)
+                .create_at(storyBox.getCreate_at())
+                .finish_at(storyBox.getFinish_at())
                 .build();
     }
 }
