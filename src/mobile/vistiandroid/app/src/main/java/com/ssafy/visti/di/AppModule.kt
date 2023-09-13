c5a79fff2518cc682f84c1ff34fa59a679cc3887
오희주 <noion0511@gmail.com>
2023-09-14 00:28:57 +0900

    feat : http 통신 코드 작성
    - https가 아닌 http도 통신 허용
    - 토큰 값 입력

--- src/mobile/vistiandroid/app/src/main/java/com/ssafy/visti/di/AppModule.kt
+++ src/mobile/vistiandroid/app/src/main/java/com/ssafy/visti/di/AppModule.kt
@@ -2,15 +2,13 @@ package com.ssafy.visti.di
 
 import com.ssafy.data.remote.PicsumApi
 import com.ssafy.data.remote.VistiApi
-import com.ssafy.data.repository.ImageRepositoryImpl
-import com.ssafy.data.repository.MemberInformationRepositoryImpl
-import com.ssafy.domain.repository.ImageRepository
 import com.ssafy.domain.model.Constants
-import com.ssafy.domain.repository.MemberInformationRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -18,6 +16,25 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            }).addInterceptor {
+                it.proceed(it.request().newBuilder().apply {
+                    addHeader(
+                        "access_token",
+                         "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJBQ0NFU1MiLCJhdXRoIjoiUk9MRV9BRE1JTiIsInVzZXJfZW1haWwiOiJzc29sbGlkYTk0QGdtYWlsLmNvbSIsImV4cCI6MTY5NTIxOTIyOX0.l1deLeNRsn71MXhYCmwclbmdEO6CzFLlqLzTdhIck6cqtTPcV145Z243wjI2VUxiWv5Pw85NrUBp-QUMQR9-KA"
+                    )
+                    addHeader(
+                        "refresh_token",
+                        "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJSRUZSRVNIIiwiZXhwIjoxNzEzMzYxNDI5fQ.CKFlc_l12RO3dUCBWFoZgkcAYAqWSc1hOPo7yhCTkmFwn8lZnksnIYlwgSPdPliY6_OR0ekrG25_kqinZpWiUA")
+                }.build())
+            }.build()
+    }
 
     @Provides
     @Singleton
@@ -31,23 +48,12 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideImageRepository(api: PicsumApi): ImageRepository {
-        return ImageRepositoryImpl(api)
-    }
-
-    @Provides
-    @Singleton
-    fun provideVistiApi(): VistiApi {
+    fun provideVistiApi(okHttpClient: OkHttpClient): VistiApi {
         return Retrofit.Builder()
+            .client(okHttpClient)
             .baseUrl(Constants.VISTI_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(VistiApi::class.java)
-    }
-
-    @Provides
-    @Singleton
-    fun provideUserInformationRepository(api: VistiApi): MemberInformationRepository {
-        return MemberInformationRepositoryImpl(api)
     }
 }
