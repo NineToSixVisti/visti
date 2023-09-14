cf2793142378f25c2d4bcb981c8d7297006516d6
강민석 <wwwttt123@naver.com>
2023-09-14 09:50:40 +0900

    feat : 스토리박스(탭1) 스토리X 레이아웃
    - 불필요한 svg 파일 삭제
    - 탭1(스토리) 스토리X 시 레이아웃

--- src/frontend/visti/src/components/Storybox/StoryboxDetail/Story.tsx
+++ src/frontend/visti/src/components/Storybox/StoryboxDetail/Story.tsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components';
+import { ReactComponent as Empty } from '../../../assets/images/story_empty.svg'
 
 const Story = () => {
+
+  const [isStory, setIsStory] = useState(false);
+
   return (
-    <div>Story</div>
+    <StoryWrap>
+    {
+      (!isStory) ?
+        <EmptyWrap>
+          {/* <img src={process.env.PUBLIC_URL + "/assets/Visti_icon.png"} alt='보호된 이미지'/> */}
+          <Empty/>
+          <p>스토리가 하나도 없어요</p>
+          <p>추억을 저장해 볼까요?</p>
+        </EmptyWrap> :
+        <StoryWrap>
+          <div></div>
+        </StoryWrap>  
+    }
+  </StoryWrap>
   )
 }
 
-export default Story
+const StoryWrap = styled.div`
+  width: 100%;
+  height: 100%;
+`
+
+const EmptyWrap = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  >svg {
+    padding-top: 120px;
+    padding-bottom: 20px;
+  }
+
+  >p {
+    color: #000;
+    text-align: center;
+    font-size: 32px;
+    font-weight: 600;
+    margin: 10px 0;
+  }
+`
+
+export default Story;
