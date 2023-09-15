0db3a17e89eef2a50394e343c0562b60e3a49843
김수찬 <tncks097@naver.com>
2023-09-15 16:39:22 +0900

    update : 지원되지 않는 토큰 403에러 뜨도록 변경

--- src/backend/src/main/java/com/spring/visti/utils/exception/ErrorCode.java
+++ src/backend/src/main/java/com/spring/visti/utils/exception/ErrorCode.java
@@ -22,7 +22,7 @@ public enum ErrorCode {
     NO_TOKEN_HEADER(HttpStatus.UNAUTHORIZED, "AUTH010", "토큰이 존재하지 않습니다."),
     JWT_INVALID(HttpStatus.UNAUTHORIZED, "AUTH011", "잘못된 JWT 서명입니다."),
     JWT_EXPIRED(HttpStatus.UNAUTHORIZED, "AUTH012", "만료된 JWT 토큰입니다."),
-    JWT_NOT_SUPPORT(HttpStatus.BAD_REQUEST, "AUTH013", "지원되지 않는 JWT 토큰입니다."),
+    JWT_NOT_SUPPORT(HttpStatus.FORBIDDEN, "AUTH013", "지원되지 않는 JWT 토큰입니다."),
     JWT_ERROR(HttpStatus.BAD_REQUEST, "AUTH014", "JWT 토큰이 잘못되었습니다."),
     NO_AUTHORIZE(HttpStatus.FORBIDDEN, "AUTH015", "권한 정보가 없는 토큰입니다."),
     NO_RT_IN_DB(HttpStatus.BAD_REQUEST, "AUTH016", "해당 리프레쉬 토큰이 DB에 존재하지 않습니다."),
