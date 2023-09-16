eff6a87f6ab022d06de7a98c55a55e8f30fc107a
김수찬 <tncks097@naver.com>
2023-09-16 15:06:00 +0900

    feat : 스케쥴러 추가

--- src/backend/src/main/java/com/spring/visti/domain/storybox/repository/StoryRepository.java
+++ src/backend/src/main/java/com/spring/visti/domain/storybox/repository/StoryRepository.java
@@ -6,6 +6,8 @@ import com.spring.visti.domain.storybox.entity.StoryBox;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -13,4 +15,7 @@ public interface StoryRepository extends JpaRepository<Story, Long> {
     Page<Story> findByMember(Member member, Pageable pageable);
 
     Page<Story> findByStoryBox(StoryBox storyBox, Pageable pageable);
+
+    @Query("SELECT COUNT(s) FROM Story s WHERE s.member.id = :memberId AND DATE(s.createdAt) = CURRENT_DATE")
+    int countTodayStoriesByMemberId(@Param("memberId") Long memberId);
 }
