ab61fda0eb48e1b64ff624fbf1491eab4e3469bf
yewon830 <wonderwall00@naver.com>
2023-09-19 17:40:22 +0900

    feat: 스토리박스 검색 api

--- src/backend/src/main/java/com/spring/visti/domain/storybox/repository/StoryBoxMemberRepository.java
+++ src/backend/src/main/java/com/spring/visti/domain/storybox/repository/StoryBoxMemberRepository.java
@@ -8,16 +8,21 @@ import com.spring.visti.domain.storybox.entity.StoryBoxMember;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StoryBoxMemberRepository extends JpaRepository<StoryBoxMember, Long> {
 
     Page<StoryBoxMember> findByMember(Member member, Pageable pageable);
+    @Query("SELECT sbm.storyBox FROM StoryBoxMember sbm WHERE sbm.member = :member AND sbm.storyBox.name LIKE %:keyword%")
+    Page<StoryBox> findJoinedByMemberAndKeyword(Member member, String keyword, Pageable pageable);
 
     Page<StoryBoxMember> findByMemberAndPosition(Member member, Position position, Pageable pageable);
 
     Optional<StoryBoxMember> findByStoryBoxIdAndMember(Long storyBoxId, Member member);
 
     boolean existsByStoryBoxIdAndMember(Long storyBoxId, Member member);
+
 }
