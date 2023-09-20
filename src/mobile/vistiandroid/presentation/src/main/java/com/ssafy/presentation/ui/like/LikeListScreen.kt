1aafab9d1dc444ec96cdbceecf490fb77a8a1d33
오희주 <noion0511@gmail.com>
2023-09-20 15:50:10 +0900

    update : 좋아요 스토리 페이징 로직 수정

--- src/mobile/vistiandroid/presentation/src/main/java/com/ssafy/presentation/ui/like/LikeListScreen.kt
+++ src/mobile/vistiandroid/presentation/src/main/java/com/ssafy/presentation/ui/like/LikeListScreen.kt
@@ -3,33 +3,30 @@ package com.ssafy.presentation.ui.like
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.paging.LoadState
+import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.ssafy.domain.model.Story
 import com.ssafy.presentation.ui.common.StoryItem
-import com.ssafy.presentation.ui.like.component.ErrorItem
-import com.ssafy.presentation.ui.like.component.LoadingItem
-import com.ssafy.presentation.ui.like.component.LoadingView
+import com.ssafy.presentation.ui.like.component.GridGroup
 import com.ssafy.presentation.ui.like.component.ToolbarWithLikeList
 import com.ssafy.presentation.ui.like.component.header
-import kotlinx.coroutines.flow.retry
+import java.text.ParseException
 import java.text.SimpleDateFormat
-import java.time.LocalDateTime
 import java.util.Date
 import java.util.Locale
 
@@ -48,80 +45,75 @@ fun LikeListScreen(viewModel: LikeListViewModel = hiltViewModel()) {
                 .padding(innerPadding),
             verticalArrangement = Arrangement.spacedBy(16.dp),
         ) {
-            val lazyLikedStories = likedStories
+            val lazyLikedStories = likedStories.collectAsLazyPagingItems()
 
-            val grouped = lazyLikedStories.itemSnapshotList.groupBy { story ->
-                val createdAtDate =
-                    SimpleDateFormat(
-                        "yyyy-MM-dd HH:mm:ss",
-                        Locale.getDefault()
-                    ).parse(
-                        story?.createdAt ?: LocalDateTime.now().toString()
-                    )
-                val formattedCreatedAt = if (createdAtDate != null) {
-                    SimpleDateFormat("yyyy년 MM월 dd일", Locale.getDefault()).format(createdAtDate)
-                } else {
-                    val currentDate = Date()
-                    SimpleDateFormat("yyyy년 MM월 dd일", Locale.getDefault()).format(currentDate)
-                }
-                formattedCreatedAt
+            val gridGroups = remember { mutableStateListOf<GridGroup>() }
+
+            LaunchedEffect(lazyLikedStories.itemSnapshotList) {
+                updateGridGroups(lazyLikedStories, gridGroups)
             }
-            LazyVerticalGrid(
-                columns = GridCells.Fixed(3)
-            ) {
-                grouped.forEach { (title, images) ->
+
+            LazyVerticalGrid(columns = GridCells.Fixed(3)) {
+                gridGroups.forEach { (title, group) ->
                     header {
                         Text(
                             title,
-                            modifier = Modifier.padding(
-                                top = 16.dp,
-                                bottom = 16.dp,
-                                start = 16.dp,
-                                end = 8.dp
-                            )
+                            modifier = Modifier
+                                .padding(
+                                    top = 16.dp,
+                                    bottom = 16.dp,
+                                    start = 16.dp,
+                                    end = 8.dp
+                                )
+                                .fillMaxWidth()
                         )
                     }
 
-                    this.items(images) { image ->
-                        if (image != null) {
-                            StoryItem(image)
-                        }
-                    }
-
-                    lazyLikedStories.apply {
-                        when {
-                            loadState.refresh is LoadState.Loading -> {
-                                item { LoadingView(modifier = Modifier.fillMaxSize()) }
-                            }
-
-                            loadState.append is LoadState.Loading -> {
-                                item { LoadingItem() }
-                            }
-
-                            loadState.refresh is LoadState.Error -> {
-                                val e = lazyLikedStories.loadState.refresh as LoadState.Error
-                                item {
-                                    ErrorItem(
-                                        message = e.error.localizedMessage!!,
-                                        modifier = Modifier.fillMaxSize(),
-                                        onClickRetry = { retry() }
-                                    )
-                                }
-                            }
-
-                            loadState.append is LoadState.Error -> {
-                                val e = lazyLikedStories.loadState.append as LoadState.Error
-                                item {
-                                    ErrorItem(
-                                        message = e.error.localizedMessage!!,
-                                        onClickRetry = { retry() }
-                                    )
-                                }
-                            }
-                        }
+                    this.items(group) { story ->
+                        StoryItem(story)
                     }
                 }
             }
         }
+    }
+}
+
+fun isNewDateGroup(prevStory: String, currentStory: String): Boolean {
+    return prevStory != currentStory
+}
+
+fun parseStoryDate(dateString: String?): Date? {
+    val format = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+    return try {
+        dateString?.let { format.parse(it) }
+    } catch (e: ParseException) {
+        null
+    }
+}
+
+fun formatStoryDate(date: Date?): String {
+    val format = SimpleDateFormat("yyyy년 MM월 dd일", Locale.getDefault())
+    return date?.let { format.format(it) } ?: format.format(Date())
+}
+
+fun updateGridGroups(lazyLikedStories: LazyPagingItems<Story>, gridGroups: MutableList<GridGroup>) {
+    var previousDate = gridGroups.lastOrNull()?.header ?: ""
+
+    for (index in 0 until lazyLikedStories.itemCount) {
+        val story = lazyLikedStories[index] ?: return
+        val storyDate = formatStoryDate(parseStoryDate(story.createdAt))
+
+        if (isNewDateGroup(previousDate, storyDate)) {
+            if (gridGroups.any { it.header == storyDate }) {
+                gridGroups.find { it.header == storyDate }?.stories?.add(story)
+            } else {
+                val newGroup = GridGroup(storyDate, mutableListOf(story))
+                gridGroups.add(newGroup)
+            }
+        } else {
+            gridGroups.last().stories.add(story)
+        }
+
+        previousDate = storyDate
     }
 }
--- src/mobile/vistiandroid/presentation/src/main/java/com/ssafy/presentation/ui/like/component/GridItem.kt
+++ src/mobile/vistiandroid/presentation/src/main/java/com/ssafy/presentation/ui/like/component/GridItem.kt
@@ -4,9 +4,14 @@ import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyGridItemScope
 import androidx.compose.foundation.lazy.grid.LazyGridScope
 import androidx.compose.runtime.Composable
+import com.ssafy.domain.model.Story
+
+data class GridGroup(val header: String, val stories: MutableList<Story>)
 
 fun LazyGridScope.header(
     content: @Composable LazyGridItemScope.() -> Unit
 ) {
     item(span = { GridItemSpan(this.maxLineSpan) }, content = content)
 }
+
+
