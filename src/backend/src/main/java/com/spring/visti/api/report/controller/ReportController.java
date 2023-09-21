eb19bd3081532f610156ffd16857f0145fe6412f
김수찬 <tncks097@naver.com>
2023-09-21 10:07:39 +0900

    feat : 스토리 및 스토리박스 내림차순 정렬 진행

--- src/backend/src/main/java/com/spring/visti/api/report/controller/ReportController.java
+++ src/backend/src/main/java/com/spring/visti/api/report/controller/ReportController.java
@@ -5,6 +5,7 @@ import com.spring.visti.api.report.service.ReportService;
 import com.spring.visti.domain.report.dto.RequestDTO.ReportBuildDTO;
 import com.spring.visti.domain.report.dto.ResponseDTO.ReportExposedDTO;
 import com.spring.visti.utils.exception.ApiException;
+import com.spring.visti.utils.urlutils.SecurePathUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 import static com.spring.visti.utils.exception.ErrorCode.NO_MEMBER_ERROR;
+import static com.spring.visti.utils.exception.ErrorCode.NO_STORY_ERROR;
 
 @RestController
 @RequestMapping("/api/report")
@@ -31,12 +33,16 @@ public class ReportController {
     @PostMapping("/storyid/{storyIds}")
     @Operation(summary = "신고 진행", description = "사용자가 신고를 진행합니다.")
     public ResponseEntity<? extends BaseResponseDTO<String>> createReport(
-            @PathVariable Long storyIds,
+            @PathVariable String storyIds,
             @RequestBody ReportBuildDTO reportInfo
     ) {
         String email = getEmail();
 
-        BaseResponseDTO<String> response = reportService.createReport(storyIds, reportInfo, email);
+        String isDecryptedStoryId = SecurePathUtil.decodeAndDecrypt(storyIds);
+        long decryptedStoryId = getDecryptedStoryId(isDecryptedStoryId);
+
+
+        BaseResponseDTO<String> response = reportService.createReport(decryptedStoryId, reportInfo, email);
         return ResponseEntity.status(response.getStatusCode()).body(response);
     }
 
@@ -48,26 +54,33 @@ public class ReportController {
         BaseResponseDTO<List<ReportExposedDTO>> response = reportService.readReports(email);
         return ResponseEntity.status(response.getStatusCode()).body(response);
     }
+
     @GetMapping("/{reportIds}")
     @Operation(summary = "처리 안된 신고 조회", description = "신고를 조회합니다.")
     public ResponseEntity<? extends BaseResponseDTO<ReportExposedDTO>> readReportDetail(
-            @PathVariable Long reportIds
+            @PathVariable String reportIds
     ) {
         String email = getEmail();
 
-        BaseResponseDTO<ReportExposedDTO> response = reportService.readReportDetail(reportIds, email);
+        String isDecryptedReportId = SecurePathUtil.decodeAndDecrypt(reportIds);
+        long decryptedReportId = getDecryptedStoryId(isDecryptedReportId);
+
+        BaseResponseDTO<ReportExposedDTO> response = reportService.readReportDetail(decryptedReportId, email);
         return ResponseEntity.status(response.getStatusCode()).body(response);
     }
 
     @PostMapping("/{reportIds}")
     @Operation(summary = "신고 처리", description = "신고를 처리합니다.")
     public ResponseEntity<? extends BaseResponseDTO<String>> updateReport(
-            @PathVariable Long reportIds,
+            @PathVariable String reportIds,
             @RequestBody Boolean process
     ) {
         String email = getEmail();
 
-        BaseResponseDTO<String> response = reportService.updateReport(reportIds, process, email);
+        String isDecryptedReportId = SecurePathUtil.decodeAndDecrypt(reportIds);
+        long decryptedReportId = getDecryptedStoryId(isDecryptedReportId);
+
+        BaseResponseDTO<String> response = reportService.updateReport(decryptedReportId, process, email);
         return ResponseEntity.status(response.getStatusCode()).body(response);
     }
 
@@ -79,4 +92,11 @@ public class ReportController {
         throw new ApiException(NO_MEMBER_ERROR);
     }
 
+    private long getDecryptedStoryId(String isDecryptedStoryId){
+        try {
+            return Long.parseLong(isDecryptedStoryId);
+        } catch (NumberFormatException e) {
+            throw new ApiException(NO_STORY_ERROR);
+        }
+    }
 }
