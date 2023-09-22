1db769475a93c80b700d1fb53690053d68c8cec3
강민석 <wwwttt123@naver.com>
2023-09-22 13:58:35 +0900

    test : 앱에서 token 받아서 사용
    - 안드로이드에서 사용하는 getToken() 함수를 사용해서 토큰 사용

--- src/frontend/visti/src/apis/utils/instance.ts
+++ src/frontend/visti/src/apis/utils/instance.ts
@@ -1,11 +1,28 @@
 import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 
+// window 객체 타입 확장
+interface MyWindow extends Window {
+  Android?: {
+    getToken: () => string;
+  };
+}
+
+declare var window: MyWindow;
+
 const baseAPI = (url: string, options?: AxiosRequestConfig): AxiosInstance => {
   return axios.create({ baseURL: url, ...options });
 }
 
 const authAPI = (url: string, options?: AxiosRequestConfig): AxiosInstance => {
-  const token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJBQ0NFU1MiLCJhdXRoIjoiUk9MRV9BRE1JTiIsInVzZXJfZW1haWwiOiJzc29sbGlkYTk0QGdtYWlsLmNvbSIsImV4cCI6MTY5NTYwNDc1M30.3GWi5uuI8N_3HBMm1xgWyAYT5rPnYEDu2fyUvMXD0sAmzOIuMp2lSlZ1u7oIKdrHSCFSrVhYQdIKeVVStZ0Uyg";
+  let token: string | undefined;
+
+  // Android에서 제공하는 getToken 함수를 사용하여 토큰을 가져옵니다.
+  if (window.Android && typeof window.Android.getToken === 'function') {
+    token = window.Android.getToken();
+  }
+
+  // const token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJBQ0NFU1MiLCJhdXRoIjoiUk9MRV9BRE1JTiIsInVzZXJfZW1haWwiOiJzc29sbGlkYTk0QGdtYWlsLmNvbSIsImV4cCI6MTY5NTYwNDc1M30.3GWi5uuI8N_3HBMm1xgWyAYT5rPnYEDu2fyUvMXD0sAmzOIuMp2lSlZ1u7oIKdrHSCFSrVhYQdIKeVVStZ0Uyg";
+  
   return axios.create({
     baseURL: url,
     headers: {
