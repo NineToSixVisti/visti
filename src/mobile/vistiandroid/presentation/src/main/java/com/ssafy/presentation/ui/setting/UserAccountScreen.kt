5e472c5823c78003b15c5f7f0d52092a45bf96d8
leehandsub <gustjq7027@naver.com>
2023-09-25 00:45:55 +0900

    update : 로그아웃 후 뒤로가기 오류 수정

--- src/mobile/vistiandroid/presentation/src/main/java/com/ssafy/presentation/ui/setting/UserAccountScreen.kt
+++ src/mobile/vistiandroid/presentation/src/main/java/com/ssafy/presentation/ui/setting/UserAccountScreen.kt
@@ -1,32 +1,26 @@
 package com.ssafy.presentation.ui.setting
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.ssafy.presentation.SignInNav
-import com.ssafy.presentation.ui.profile.ProfileViewModel
+import com.ssafy.presentation.ui.common.VistiDialog
 import com.ssafy.presentation.ui.setting.component.BackToolbar
 import com.ssafy.presentation.ui.setting.component.DetailSettingButton
-import com.ssafy.presentation.ui.common.VistiDialog
 import com.ssafy.presentation.ui.theme.Black
 import com.ssafy.presentation.ui.theme.PrimaryColor
 import com.ssafy.presentation.ui.theme.White
 
 @Composable
 fun UserAccountScreen(
-    navController: NavController, viewModel: UserAccountViewModel = hiltViewModel()
+    navController: NavController, viewModel: UserAccountViewModel = hiltViewModel(),
 ) {
     val signOutState = remember { mutableStateOf(false) }
     val logOutState = remember { mutableStateOf(false) }
@@ -69,7 +63,7 @@ fun UserAccountScreen(
                         viewModel.removeToken()
 
                         navController.navigate(SignInNav.SignIn.route) {
-                            popUpTo(navController.graph.startDestinationId) {
+                            popUpTo(navController.graph.id) {
                                 inclusive = true
                             }
                             launchSingleTop = true
