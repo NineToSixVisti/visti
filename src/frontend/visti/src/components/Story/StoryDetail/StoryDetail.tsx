aa614b1f54c66b4f5b663855168459f3cb8b9015
김상진 <tkdwls0418@naver.com>
2023-09-25 16:00:02 +0900

    feat: 신고버튼 및 신고 기능 추가

--- src/frontend/visti/src/components/Story/StoryDetail/StoryDetail.tsx
+++ src/frontend/visti/src/components/Story/StoryDetail/StoryDetail.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { authInstance } from '../../../apis/utils/instance';
-import styled from 'styled-components';
-import { ReactComponent as BackButtonIcon } from '../../../assets/images/storydetailclose_button.svg';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { authInstance } from "../../../apis/utils/instance";
+import styled from "styled-components";
+import { ReactComponent as BackButtonIcon } from "../../../assets/images/storydetailclose_button.svg";
+import { useNavigate } from "react-router-dom";
+import BasicModal from "./ReportButton";
 
 interface StoryData {
   id: number;
@@ -15,7 +16,6 @@ interface StoryData {
     nickname: string;
     profilePath: string | null;
     status: boolean;
-    
   };
 }
 
@@ -23,16 +23,16 @@ const InfoContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  margin-left: 8px; 
+  margin-left: 8px;
 `;
 
 const ProfileContainer = styled.div`
   position: absolute;
   bottom: 2%;
-  left: 25%; 
+  left: 25%;
   display: flex;
   align-items: center;
-  transform: translateX(-50%); 
+  transform: translateX(-50%);
 `;
 
 const Nickname = styled.span`
@@ -40,7 +40,7 @@ const Nickname = styled.span`
   font-weight: bold;
   color: black;
   text-shadow: 0px 0px 5px rgba(0, 0, 0, 0.4);
-  margin-bottom: 8px; 
+  margin-bottom: 8px;
 `;
 
 const Container = styled.div`
@@ -48,16 +48,15 @@ const Container = styled.div`
   width: 100vw;
   height: 100vh;
   overflow: hidden;
-  background-color:#d3d3d3;
+  background-color: #d3d3d3;
 `;
 
 const MainImage = styled.img`
   width: 100%;
-  height:90vh;
+  height: 90vh;
   object-fit: contain;
-  border-radius: 15px; 
+  border-radius: 15px;
 `;
-
 
 const ProfileImage = styled.img`
   width: 50px;
@@ -66,23 +65,30 @@ const ProfileImage = styled.img`
   margin-right: 8px;
 `;
 
-
 const CreatedAt = styled.span`
   font-size: 14px;
   color: black;
   text-shadow: 0px 0px 5px rgba(0, 0, 0, 0.4);
   margin-left: 10px;
 `;
-const BackButton = styled.button`
+const ButtonContainer = styled.div`
   position: absolute;
   top: 10px;
   right: 10px;
-  background-color: transparent; 
-  border: none;
-  cursor: pointer;
-  z-index: 10; 
+  display: flex;
+  align-items: center;
 `;
 
+const BackButton = styled.button`
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  margin-right: 10px;
+  margin-top: 9px;
+`;
+const BasicModalStyled = styled(BasicModal)`
+  background-color: transparent;
+`;
 
 function StoryDetail() {
   const { id } = useParams<{ id?: string }>();
@@ -95,11 +101,12 @@ function StoryDetail() {
   useEffect(() => {
     if (!id) return;
 
-    authInstance.get(`story/${id}`)
-      .then(response => {
+    authInstance
+      .get(`story/${id}`)
+      .then((response) => {
         setStoryData(response.data.detail);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("Failed to fetch story data:", error);
       });
   }, [id]);
@@ -108,17 +115,25 @@ function StoryDetail() {
 
   function formatDate(dateString: string): string {
     const date = new Date(dateString);
-    return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
+    return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(
+      2,
+      "0"
+    )}-${String(date.getDate()).padStart(2, "0")}`;
   }
 
   return (
     <Container>
-     <BackButton onClick={handleBack}>
-        <BackButtonIcon /> 
-      </BackButton>
+      <ButtonContainer>
+        <BasicModal storyId={id} />
+        <BackButton onClick={handleBack}>
+          <BackButtonIcon />
+        </BackButton>
+      </ButtonContainer>
       <MainImage src={storyData.mainFilePath} alt="Story" />
       <ProfileContainer>
-        {storyData.member.profilePath && <ProfileImage src={storyData.member.profilePath} alt="Profile" />}
+        {storyData.member.profilePath && (
+          <ProfileImage src={storyData.member.profilePath} alt="Profile" />
+        )}
         <InfoContainer>
           <Nickname>{storyData.member.nickname}</Nickname>
           <CreatedAt>{formatDate(storyData.createdAt)}</CreatedAt>
