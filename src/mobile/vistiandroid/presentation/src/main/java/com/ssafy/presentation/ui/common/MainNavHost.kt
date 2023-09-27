129f1d6bcb37293c65b3c6d733746dd08b70583e
leehandsub <gustjq7027@naver.com>
2023-09-27 21:58:48 +0900

    feat : 딥링크 받아오기

--- src/mobile/vistiandroid/presentation/src/main/java/com/ssafy/presentation/ui/common/MainNavHost.kt
+++ src/mobile/vistiandroid/presentation/src/main/java/com/ssafy/presentation/ui/common/MainNavHost.kt
@@ -1,20 +1,25 @@
 package com.ssafy.presentation.ui.common
 
 import android.content.Context
-import android.util.Log
+import android.content.Intent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import androidx.navigation.navDeepLink
 import com.ssafy.presentation.MainNav
-import com.ssafy.presentation.MainScreen
 import com.ssafy.presentation.SettingNav
 import com.ssafy.presentation.SignInNav
 import com.ssafy.presentation.ui.home.HomeScreen
-import com.ssafy.presentation.ui.like.LikeListScreen
 import com.ssafy.presentation.ui.profile.ProfileScreen
 import com.ssafy.presentation.ui.setting.InformationScreen
 import com.ssafy.presentation.ui.setting.NotificationSettingScreen
@@ -33,7 +38,7 @@ fun MainNavHost(
     innerPaddings: PaddingValues,
     navController: NavHostController,
     context: Context
-    ) {
+) {
     NavHost(
         modifier = Modifier,
         navController = navController,
@@ -45,9 +50,30 @@ fun MainNavHost(
         composable(MainNav.Memory.route) {
             StoryScreen()
         }
-        composable(MainNav.Like.route) {
-            LikeListScreen()
+        composable(MainNav.Memory.route){
+            StoryScreen()
         }
+        composable(
+            MainNav.Memory.route, deepLinks = listOf(navDeepLink {
+                uriPattern = "https://www.visti.com/{id}"
+                action = Intent.ACTION_VIEW
+            }),
+            arguments = listOf(navArgument("id") {
+                type = NavType.StringType
+                defaultValue = ""
+            })
+
+        ) {
+            entry ->
+            val id = entry.arguments?.getString("id")
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "The id is $id")
+            }
+        }
+
         composable(MainNav.Profile.route) {
             ProfileScreen(navController = navController)
         }
