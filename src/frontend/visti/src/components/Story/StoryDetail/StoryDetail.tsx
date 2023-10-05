3265b55cd4e83f1dfc5b3e8e27f92e778f9f7b70
김상진 <tkdwls0418@naver.com>
2023-10-05 15:55:22 +0900

    update: 신고버튼 클로즈 버튼 색상 수정

--- src/frontend/visti/src/components/Story/StoryDetail/StoryDetail.tsx
+++ src/frontend/visti/src/components/Story/StoryDetail/StoryDetail.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { authInstance } from "../../../apis/utils/instance";
 import styled from "styled-components";
-import { ReactComponent as BackButtonIcon } from "../../../assets/images/storydetailclose_button.svg";
+import { ReactComponent as BackButtonIcon } from "../../../assets/images/closeblack-button.svg";
 import { ReactComponent as HeartButton } from "../../../assets/images/heartbutton.svg";
 import { ReactComponent as Heartwhite } from "../../../assets/images/heartwhite.svg";
 import { useNavigate } from "react-router-dom";
